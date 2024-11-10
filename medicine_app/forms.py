c170eb35250e35b58744dceef1fc879ce247bb69
animesh <sethanimesh@hotmail.com>
2024-11-10 15:42:50 +0530

    Form for image uploads

--- medicine_app/forms.py
+++ medicine_app/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Medicine
+from .models import Medicine, MedicineScan
 
 class MedicineForm(forms.ModelForm):
     class Meta:
         model = Medicine
         fields = ['name', 'manufacturer', 'dosage', 'expiration_date']
+
+class MedicineScanForm(forms.ModelForm):
+    class Meta:
+        model = MedicineScan
+        fields = ['image']
